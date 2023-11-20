35afdd929e9113f5e4c7ac68afefdc76bd3c58e1
th-release <cth.release@gmail.com>
2023-11-21 08:43:07 +0900

    fix: bug fix

--- src/system/system.module.ts
+++ src/system/system.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SystemController } from './system.controller';
 import { SystemService } from './system.service';
-import { SystemGateway } from './system.gateway';
 
 @Module({
   imports: [
