b311c5fa0d321e5dda618a7450273731ed6c9ccc
th-release <cth.release@gmail.com>
2023-11-22 11:38:58 +0900

    chore: config

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -1,12 +1,11 @@
 export default () => ({
   DATABASE_HOST: process.env.DATABASE_HOST,
-  DATABASE_PORT: parseInt(process.env.DATABASE_PORT) || 8000,
+  DATABASE_PORT: parseInt(process.env.DATABASE_PORT) || 3306,
   DATABASE_USERNAME: process.env.DATABASE_USERNAME,
   DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
   DATABASE_SCHEMA: process.env.DATABASE_SCHEMA,
-  TYPEORM_SYBCHRONIZE: process.env.MODE === "DEV" ? true : false,
-  MODE: process.env.MODE,
-  REDIS_HOST: process.env.REDIS_HOST,
-  REDIS_PORT: parseInt(process.env.REDIS_PORT) || 8080,
-  REDIS_TTL: parseInt(process.env.REDIS_TTL) || 100000,
+  DATABASE_LOGGING: process.env.DATABASE_LOGGING === "true" ? true : false,
+  DATABASE_SYNCHRONIZE: process.env.DATABASE_SYNCHRONIZE === "true" ? true : false,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_ISSUER: process.env.JWT_ISSUER,
 })
