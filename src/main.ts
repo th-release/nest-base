050926e903356df496555c90ec665aa15e575ccb
th-release <cth.release@gmail.com>
2023-11-22 11:50:35 +0900

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle("title")
+    .setDescription("version. 1")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // dto를 위해 useGlobalPipes 추가
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,7 +26,8 @@ async function bootstrap() {
     })
   );
 
-  app.setGlobalPrefix('api');
+  SwaggerModule.setup('/api-docs', app, document);
+
   app.enableCors();
   await app.listen(3000);
 }
